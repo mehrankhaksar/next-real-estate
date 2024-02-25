b16251b9038e5f3dcdaf544a34788ed99e391a26
Mehran Khaksar <59496736+mehrankhaksar@users.noreply.github.com>
2024-02-25 11:52:00 +0330

    some changes

--- src/app/advertisements/page.js
+++ src/app/advertisements/page.js
@@ -1,4 +1,5 @@
-export const dynamic = "force-static";
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
 
 import connectDB from "@/utils/connectDB";
 import Advertisement from "@/models/Advertisement";
