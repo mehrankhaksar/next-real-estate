0e5ce8ef734f4405221a9d9ae226a0c050255867
Titty <mehran9326@gmail.com>
2024-05-30 23:06:25 +0330

    some styles changed in advertisement card

--- src/components/modules/AdvertisementCard.jsx
+++ src/components/modules/AdvertisementCard.jsx
@@ -8,13 +8,12 @@ import {
   CardFooter,
 } from "../ui/card";
 import { Separator } from "../ui/separator";
+import { Button } from "../ui/button";
 
 import { Home, Building, Store, Building2, MapPin } from "lucide-react";
 
 import { e2p, findName, sp } from "@/utils/helperFunctions";
 import { provinces, cities } from "@/constants/lists";
-
-import CustomButton from "./CustomButton";
 
 const AdvertisementCard = ({ title, category, province, city, price, _id }) => {
   const categoriesIcons = {
@@ -25,8 +24,8 @@ const AdvertisementCard = ({ title, category, province, city, price, _id }) => {
   };
 
   return (
-    <Card className="shadow shadow-primary">
-      <CardHeader className="px-3 sm:px-6">
+    <Card className="shadow-md shadow-primary">
+      <CardHeader>
         <div className="flex justify-between items-center gap-5 text-primary">
           <CardTitle className="font-extrabold">{e2p(title)}</CardTitle>
           <span className="bg-primary/25 p-1.5 rounded-md">
@@ -34,23 +33,29 @@ const AdvertisementCard = ({ title, category, province, city, price, _id }) => {
           </span>
         </div>
       </CardHeader>
-      <Separator className="bg-primary mb-1.5" />
-      <CardContent className="font-semibold text-muted-foreground p-2.5">
-        <div className="flex items-center gap-1">
+      <Separator className="bg-primary" />
+      <CardContent className="font-semibold text-muted-foreground py-2.5">
+        <div className="flex items-center gap-0.5">
           <MapPin size={18} />
           {findName(provinces, province)}, {findName(cities, city)}
         </div>
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-0.5">
           <span className="font-extrabold text-secondary-foreground">
             {sp(price)}
           </span>
           تومان
         </div>
       </CardContent>
-      <CardFooter className="p-2.5">
-        <Link className="w-full" href={`/advertisements/${_id}`}>
-          <CustomButton containerStyles="text-sm">مشاهده آگهی</CustomButton>
-        </Link>
+      <CardFooter>
+        <Button
+          asChild
+          className="font-semibold dark:text-secondary-foreground"
+          type="button"
+        >
+          <Link className="w-full" href={`/advertisements/${_id}`}>
+            مشاهده آگهی
+          </Link>
+        </Button>
       </CardFooter>
     </Card>
   );
