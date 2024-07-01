d04f49e000dbf225ad9f12962ff9263a622789bf
Titty <mehran9326@gmail.com>
2024-07-02 01:21:42 +0330

    fixed error

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -2,6 +2,6 @@
 
 import { ThemeProvider as NextThemeProvider } from "next-themes";
 
-export default function ({ children, ...props }) {
+export default function ThemeProvider({ children, ...props }) {
   return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
 }
